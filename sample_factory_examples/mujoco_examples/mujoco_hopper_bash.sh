2edd0426ff61be5f4ab9096b5fc215b308a802d7
Zhehui Huang <zhehuihu@usc.edu>
2022-12-04 21:12:53 -0800

    Add scripts for mujoco pendulum and hopper

---
+++ sample_factory_examples/mujoco_examples/mujoco_hopper_bash.sh
@@ -0,0 +1,10 @@
+python -m sample_factory.runner.run \
+--backend=slurm \
+--experiment_suffix=slurm \
+--slurm_gpus_per_job=1 \
+--slurm_cpus_per_gpu=16 \
+--pause_between=1 \
+--slurm_print_only=False \
+--slurm_sbatch_template=/home/zhehui/reward_decrease/slurm/sbatch_timeout.sh \
+--slurm_workdir=/home/zhehui/reward_decrease/slurm/mujoco/hopper \
+--run=sample_factory_examples.mujoco_examples.mujoco_hopper
