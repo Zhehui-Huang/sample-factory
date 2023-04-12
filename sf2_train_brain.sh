1c3c31d88632ad3714d4bbdb110a7608c0c9a549
Zhehui Huang <zhehuihu@usc.edu>
2023-04-11 19:03:05 -0700

    modify slurm running script

--- sf2_train_brain.sh
+++ sf2_train_brain.sh
@@ -1,5 +1,5 @@
 python -m sample_factory.launcher.run \
---run=sf_examples.mujoco.runs \
+--run=sf_examples.mujoco.runs.ant \
 --backend=slurm --slurm_workdir=sf2_slurm_output \
 --experiment_suffix=slurm --pause_between=1 \
 --slurm_gpus_per_job=1 --slurm_cpus_per_gpu=16 \
