5e73dfd346275d3b0426427e2de2dde0796432a2
Zhehui Huang <zhehuihu@usc.edu>
2023-04-13 14:34:59 -0700

    modify dmlab30 pbt

--- runs/dmlab/baseline.py
+++ runs/dmlab/baseline.py
@@ -11,7 +11,7 @@ DMLAB30_BASELINE_CLI = (
 
 DMLAB30_PBT_BASELINE_CLI = (
     'python -m sf_examples.dmlab.train_dmlab --env=dmlab_30 --train_for_env_steps=10000000000 --gamma=0.99 '
-    '--use_rnn=True --num_workers=90 --num_envs_per_worker=12 --num_epochs=1 --rollout=32 --recurrence=32 '
+    '--use_rnn=True --num_workers=72 --num_envs_per_worker=12 --num_epochs=1 --rollout=32 --recurrence=32 '
     '--batch_size=2048 --benchmark=False --max_grad_norm=0.0 --dmlab_renderer=software '
     '--decorrelate_experience_max_seconds=120 --encoder_conv_architecture=resnet_impala '
     '--encoder_conv_mlp_layers=512 --nonlinearity=relu --rnn_type=lstm --dmlab_extended_action_set=True '
