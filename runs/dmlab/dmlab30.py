5329eaa384a0b02446c9f0f24d79951e094d2945
Zhehui Huang <zhehuihu@usc.edu>
2023-04-12 21:13:25 -0700

    Support dmlab 30

---
+++ runs/dmlab/dmlab30.py
@@ -0,0 +1,19 @@
+from sample_factory.launcher.run_description import RunDescription, Experiment, ParamGrid
+from runs.dmlab.baseline import DMLAB30_BASELINE_CLI
+
+_params = ParamGrid([
+    ('seed', [0000, 1111, 2222, 3333]),
+])
+
+DMLAB30_CLI = DMLAB30_BASELINE_CLI + (
+    ' --with_wandb=True --wandb_project=stabilized-rl '
+    '--wandb_group=dmlab_sf_origin --wandb_user=resl-mixppo'
+)
+
+_experiment = Experiment(
+    'dmlab_sf_origin',
+    DMLAB30_CLI,
+    _params.generate_params(randomize=False),
+)
+
+RUN_DESCRIPTION = RunDescription('sf_ori_dmlab', experiments=[_experiment])
