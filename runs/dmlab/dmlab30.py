1d16ae773605431871e1e58b1c0b724a8d037397
Zhehui Huang <zhehuihu@usc.edu>
2023-04-14 13:43:19 -0700

    Modify dmlab runner script

--- runs/dmlab/dmlab30.py
+++ runs/dmlab/dmlab30.py
@@ -2,16 +2,16 @@ from sample_factory.launcher.run_description import RunDescription, Experiment, 
 from runs.dmlab.baseline import DMLAB30_BASELINE_CLI
 
 _params = ParamGrid([
-    ('seed', [0000, 1111, 2222, 3333]),
+    ('seed', [0000, 1111]),
 ])
 
 DMLAB30_CLI = DMLAB30_BASELINE_CLI + (
     ' --with_wandb=True --wandb_project=stabilized-rl '
-    '--wandb_group=dmlab_sf_origin_v2 --wandb_user=resl-mixppo'
+    '--wandb_group=dmlab_sf_origin_v3 --wandb_user=resl-mixppo'
 )
 
 _experiment = Experiment(
-    'dmlab_sf_origin_v2',
+    'dmlab_sf_origin_v3',
     DMLAB30_CLI,
     _params.generate_params(randomize=False),
 )
