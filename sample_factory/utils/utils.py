b8c02eaf6ca62865ff4b6af4ce39aeca0721e8af
Zhehui Huang <zhehuihu@usc.edu>
2023-04-14 19:39:31 -0700

    Try to fix PBT issue: v2

--- sample_factory/utils/utils.py
+++ sample_factory/utils/utils.py
@@ -185,6 +185,34 @@ def retry(times, exceptions):
     return decorator
 
 
+def retry_run(times, exceptions):
+    """
+    Retry Decorator https://stackoverflow.com/a/64030200/1645784
+    Retries the wrapped function/method `times` times if the exceptions listed
+    in ``exceptions`` are thrown
+    :param times: The number of times to repeat the wrapped function/method
+    :type times: Int
+    :param exceptions: Lists of exceptions that trigger a retry attempt
+    :type exceptions: Tuple of Exceptions
+    """
+
+    def decorator(func):
+        def _run(*args, **kwargs):
+            attempt = 0
+            while attempt < times:
+                try:
+                    return func(*args, **kwargs)
+                except exceptions:
+                    log.warning(f"Exception thrown when attempting to run {func}, attempt {attempt} out of {times}")
+                    time.sleep(min(2**attempt, 10))
+                    attempt += 1
+
+            return func(*args, **kwargs)
+
+        return _run
+
+    return decorator
+
 # CLI args
 
 
