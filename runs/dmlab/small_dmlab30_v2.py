5e6f87f20422ce83d2e1da6936ae92a9cb047962
Zhehui Huang <zhehuihu@usc.edu>
2023-04-20 23:37:12 -0700

    Modify scripts

--- runs/dmlab/small_dmlab30_v2.py
+++ runs/dmlab/small_dmlab30_v2.py
@@ -3,18 +3,18 @@ from runs.dmlab.baseline import SMALL_NUM_ENV_DMLAB30_BASELINE_CLI
 
 _params = ParamGrid([
     ('seed', [0000]),
-    ('target_kl', [0.01, 0.05]),
-    ('kl_loss_coeff_lr', [0.5, 3.0, 10.0]),
+    ('target_kl', [0.001, 0.05]),
+    ('kl_loss_coeff_lr', [0.5, 3.0]),
     ('kl_loss_coeff_momentum', [0.99999]),
 ])
 
 DMLAB30_CLI = SMALL_NUM_ENV_DMLAB30_BASELINE_CLI + (
-    ' --MIN_KL_LOSS_COEFF=0.001 --start_kl_steps=0 '
+    ' --sparse_second_loop=True --MIN_KL_LOSS_COEFF=0.001 --start_kl_steps=0 '
     '--with_wandb=True --wandb_project=stabilized-rl --wandb_group=small_env_dmlab_sf_xppo --wandb_user=resl-mixppo'
 )
 
 _experiment = Experiment(
-    'small_dmlab_sf_xppo_target_kl_search_v6',
+    'small_dmlab_sf_xppo_target_kl_search_v7',
     DMLAB30_CLI,
     _params.generate_params(randomize=False),
 )
