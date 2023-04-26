71a32456c39f01aa31896b3c2913fb011a6b1e7b
Zhehui Huang <zhehuihu@usc.edu>
2023-04-25 18:44:24 -0700

    Minor modifications

--- runs/dmlab/dmlab30.py
+++ runs/dmlab/dmlab30.py
@@ -2,12 +2,12 @@ from sample_factory.launcher.run_description import RunDescription, Experiment, 
 from runs.dmlab.baseline import DMLAB30_BASELINE_CLI
 
 _params = ParamGrid([
-    ('seed', [0000, 1111, 2222, 3333]),
-    ('second_loop_max', [100, 200]),
+    ('seed', [0000]),
+    ('MAX_KL_LOSS_COEFF', [500, 1000]),
 ])
 
 DMLAB30_CLI = DMLAB30_BASELINE_CLI + (
-    ' --sparse_second_loop=True --target_kl=0.001 --kl_loss_coeff_lr=3.0 --MIN_KL_LOSS_COEFF=0.001 --start_kl_steps=0 '
+    ' --second_loop_max=100 --sparse_second_loop=True --target_kl=0.001 --kl_loss_coeff_lr=3.0 --MIN_KL_LOSS_COEFF=0.001 --start_kl_steps=0 '
     '--with_wandb=True --wandb_project=stabilized-rl --wandb_group=dmlab_sf_xppo_tkl_001 --wandb_user=resl-mixppo'
 )
 
