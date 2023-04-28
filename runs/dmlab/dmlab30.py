cb4418089c089e8efd3bf59399289f07720a2815
Zhehui Huang <zhehuihu@usc.edu>
2023-04-28 01:53:22 -0700

    Modify runner scripts

--- runs/dmlab/dmlab30.py
+++ runs/dmlab/dmlab30.py
@@ -2,16 +2,17 @@ from sample_factory.launcher.run_description import RunDescription, Experiment, 
 from runs.dmlab.baseline import DMLAB30_BASELINE_CLI
 
 _params = ParamGrid([
-    ('seed', [0000, 1111, 2222, 3333]),
+    ('seed', [0000, 1111]),
+    ('num_batches_per_epoch', [2, 4]),
 ])
 
 DMLAB30_CLI = DMLAB30_BASELINE_CLI + (
-    ' --target_kl=0.001 --kl_loss_coeff_lr=3.0 --MIN_KL_LOSS_COEFF=0.001 --start_kl_steps=0 '
-    '--with_wandb=True --wandb_project=stabilized-rl --wandb_group=dmlab_sf_xppo_tkl_001 --wandb_user=resl-mixppo'
+    ' --target_kl=0.001 --kl_loss_coeff_lr=0.1 --MIN_KL_LOSS_COEFF=0.001 --start_kl_steps=0 '
+    '--with_wandb=True --wandb_project=stabilized-rl --wandb_group=dmlab_sf_xppo_minibatch --wandb_user=resl-mixppo'
 )
 
 _experiment = Experiment(
-    'dmlab_sf_xppo_target_kl_001',
+    'dmlab_sf_xppo_minibatch',
     DMLAB30_CLI,
     _params.generate_params(randomize=False),
 )
