b0544e8837f068be7b235cf7f73e74aec7af4106
Zhehui Huang <zhehuihu@usc.edu>
2023-10-29 10:29:19 -0700

    Add a runner script

---
+++ runs/dmlab/dmlab_collect_good_objects.py
@@ -0,0 +1,21 @@
+from sample_factory.launcher.run_description import RunDescription, Experiment, ParamGrid
+from runs.dmlab.baseline import DMLAB30_BASELINE_CLI
+
+_params = ParamGrid([
+    ('seed', [0000, 1111, 2222, 3333]),
+    ('batch_size', [1024]),
+])
+
+DMLAB30_CLI = DMLAB30_BASELINE_CLI + (
+    ' --env=dmlab_collect_good_objects '
+    '--train_for_env_steps=100000000 --with_wandb=True --wandb_project=stabilized-rl '
+    '--wandb_group=sf-origin_dmlab_collect_good_objects --wandb_user=resl-mixppo'
+)
+
+_experiment = Experiment(
+    'dmlab_collect_good_objects_v1',
+    DMLAB30_CLI,
+    _params.generate_params(randomize=False),
+)
+
+RUN_DESCRIPTION = RunDescription('origin_sf', experiments=[_experiment])
